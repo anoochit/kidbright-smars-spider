fd9dcac4dde6e68418a6b5bfaeee66d11fceda49
Anuchit Chalothorn <anoochit@gmail.com>
2018-11-14 14:05:54 +0700

    Update blocks.js

--- smars/spider/blocks.js
+++ smars/spider/blocks.js
@@ -8,13 +8,13 @@ Blockly.Blocks['spider.forward'] = {
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
         this.setColour(160);
-     this.setTooltip(Blockly.Msg.SPIDER_FORWARD_TOOLTIP);
-     this.setHelpUrl(Blockly.Msg.SPIDER_FORWARD_HELPURL);
+        this.setTooltip(Blockly.Msg.SPIDER_FORWARD_TOOLTIP);
+        this.setHelpUrl(Blockly.Msg.SPIDER_FORWARD_HELPURL);
     }
-  };
+};
 
 
-  Blockly.Blocks['spider.backward'] = {
+Blockly.Blocks['spider.backward'] = {
     init: function() {
         this.appendDummyInput()
             .appendField(Blockly.Msg.SPIDER_BACKWARD_TITLE);
@@ -24,7 +24,52 @@ Blockly.Blocks['spider.forward'] = {
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
         this.setColour(160);
-     this.setTooltip(Blockly.Msg.SPIDER_BACKWARD_TOOLTIP);
-     this.setHelpUrl(Blockly.Msg.SPIDER_BACKWARD_HELPURL);
+        this.setTooltip(Blockly.Msg.SPIDER_BACKWARD_TOOLTIP);
+        this.setHelpUrl(Blockly.Msg.SPIDER_BACKWARD_HELPURL);
     }
-  };
+};
+
+Blockly.Blocks['spider.trun_left'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(Blockly.Msg.SPIDER_TURN_LEFT_TITLE);
+        this.appendValueInput("STEP")
+            .setCheck("Number");
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(160);
+        this.setTooltip(Blockly.Msg.SPIDER_TURN_LEFT_TOOLTIP);
+        this.setHelpUrl(Blockly.Msg.SPIDER_TURN_LEFT_HELPURL);
+    }
+};
+
+Blockly.Blocks['spider.trun_right'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(Blockly.Msg.SPIDER_TURN_RIGHT_TITLE);
+        this.appendValueInput("STEP")
+            .setCheck("Number");
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(160);
+        this.setTooltip(Blockly.Msg.SPIDER_TURN_RIGHT_TOOLTIP);
+        this.setHelpUrl(Blockly.Msg.SPIDER_TURN_RIGHT_HELPURL);
+    }
+};
+
+Blockly.Blocks['spider.dance'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(Blockly.Msg.SPIDER_DANCE_TITLE);
+        this.appendValueInput("STEP")
+            .setCheck("Number");
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(160);
+        this.setTooltip(Blockly.Msg.SPIDER_DANCE_TOOLTIP);
+        this.setHelpUrl(Blockly.Msg.SPIDER_DANCE_HELPURL);
+    }
+};
