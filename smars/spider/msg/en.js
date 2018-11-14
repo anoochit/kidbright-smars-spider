43612d9c1470999dfb399d573e5bc2c312a6f523
Anuchit Chalothorn <anoochit@gmail.com>
2018-11-14 17:53:35 +0700

    Update en.js

--- smars/spider/msg/en.js
+++ smars/spider/msg/en.js
@@ -5,3 +5,11 @@ Blockly.Msg.SPIDER_FORWARD_HELPURL="";
 Blockly.Msg.SPIDER_BACKWARD_TITLE="Backword";
 Blockly.Msg.SPIDER_BACKARD_TOOLTIP="Move backward";
 Blockly.Msg.SPIDER_BACKWARD_HELPURL="";
+
+Blockly.Msg.SPIDER_TURN_LEFT_TITLE="Turn Left";
+Blockly.Msg.SPIDER_TURN_LEFT_TOOLTIP="Turn Left";
+Blockly.Msg.SPIDER_TURN_LEFT_HELPURL="";
+
+Blockly.Msg.SPIDER_TURN_RIGHT_TITLE="Turn Right";
+Blockly.Msg.SPIDER_TURN_RIGHT_TOOLTIP="Turn Right";
+Blockly.Msg.SPIDER_TURN_RIGHT_HELPURL="";
