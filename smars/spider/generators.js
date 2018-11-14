2654dd168bb76fe419b3539e2cc407987c9ba57a
Anuchit Chalothorn <anoochit@gmail.com>
2018-11-14 13:55:37 +0700

    Update generators.js
    add more function for turn left, turn right

--- smars/spider/generators.js
+++ smars/spider/generators.js
@@ -5,3 +5,19 @@ Blockly.JavaScript['spider.forward'] = function(block) {
 Blockly.JavaScript['spider.backward'] = function(block) {
     return 'DEV_IO.SPIDER().backward();\n';
 };
+
+Blockly.JavaScript['spider.turn_left'] = function(block) {
+    return 'DEV_IO.SPIDER().turn_left();\n';
+};
+
+Blockly.JavaScript['spider.turn_right'] = function(block) {
+    return 'DEV_IO.SPIDER().turn_right();\n';
+};
+
+Blockly.JavaScript['spider.dance'] = function(block) {
+    return 'DEV_IO.SPIDER().dance();\n';
+};
+
+Blockly.JavaScript['spider.greeting'] = function(block) {
+    return 'DEV_IO.SPIDER().greeting();\n';
+};
